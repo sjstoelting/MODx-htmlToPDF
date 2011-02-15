f5f649fce49da0f93f9778cac5f21d5b494c3d09
sjstoelting <mail@stefanie-stoelting.de>
2011-02-15 07:39:16 +0100

    First commit

---
+++ assets/snippets/htmlToPDF/snippet.getCurrentDocID.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Snippet Name: getCurrentDocID
+ * Description: <strong>0.1.alpha1</strong> Get the current document ID
+ *
+ * @name getCurrentDocID
+ *
+ * @author Stefanie Janine Stoelting, mail@stefanie-stoelting.de
+ * @link http://code.google.com/p/htmlToPDF/
+ * @link http://www.stefanie-stoelting.de/phptohtml-news.html
+ * @link http://www.tcpdf.org/
+ * @package htmlToPDF
+ * @license LGPL
+ * @since 2011/02/15
+ * @version 0.1.alpha1
+ */
+
+return $modx->documentObject['id'];
+?>
